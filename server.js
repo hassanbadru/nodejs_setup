0cc5d32064a133b1167acf8503b59e863537ae99
Hassan Badru <hassan.badru@gmail.com>
2017-03-31 08:38:41 -0400

    routing & api with express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import config from './config';
 import express from 'express';
-import fs from 'fs';
+//import fs from 'fs';
+import apiRouter from './api';
 
 const server = express();
 
@@ -8,11 +9,15 @@ server.get('/', (req, res) => {
     res.send('Hello Express\n');
 });
 
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+
+/*
 server.get('/about', (req, res) => {
     fs.readFile('./about.html', (err, data) => {
           res.send(data.toString());
     });
-});
+});*/
 
 server.listen(config.port, () => {
       console.info('Express listening on port', config.port);
