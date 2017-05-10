c43700dec965873f0155b6f0a4e5941ab073849e
Hassan Badru <hassan.badru@gmail.com>
2017-05-10 19:13:00 -0400

    using jsx
    jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' : 'red';
+
 ReactDOM.render(
-  React.createElement('h2', null, 'Hello React'),
+  <h2 className="text-center">
+      Hello React with JSX
+  </h2>,
   document.getElementById('root')
 );
